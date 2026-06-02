6650ffe748159043d90106bcd982807467af95c3
Jotham Den M. Baquiran <jothamdenmbaquirandevwork@gmail.com>
2026-06-02 15:16:25 +0800

    fix: use consistent naming for DOM elements and IDs

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const noteTitle = document.getElementById("noteTitle");
+const noteContent = document.getElementById("noteContent");
+const addNoteBtn = document.getElementById("addNoteBtn");
+const notesContainer = document.getElementById("notesContainer");
