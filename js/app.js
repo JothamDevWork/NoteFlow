e5d519e6d8ef61fc73b52756095363f161f06f50
Jotham Den M. Baquiran <jothamdenmbaquirandevwork@gmail.com>
2026-06-04 20:06:47 +0800

    feat: added  search and filter

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 import * as noteOperations from "./noteOperations.js";
+import * as searchFilterOperations from "./searchFilterOperations.js";
 const noteTitle = document.getElementById("noteTitle");
 const noteContent = document.getElementById("noteContent");
 const addNoteBtn = document.getElementById("addNoteBtn");
 const notesContainer = document.getElementById("notesContainer");
+const searchBar = document.getElementById("searchBar");
+const searchBtn = document.getElementById("searchBtn");
 
 addNoteBtn.addEventListener("click", (event) => {
   event.preventDefault();
@@ -11,5 +14,12 @@ addNoteBtn.addEventListener("click", (event) => {
     noteTitle,
     noteContent,
     notesContainer,
+  });
+});
+
+searchBtn.addEventListener("click", () => {
+  searchFilterOperations.searchFilterBar({
+    notesContainer,
+    searchBar,
   });
 });
