2ccae5e6d59169257152dce957e13d94a9dbe91d
Jotham Den M. Baquiran <jothamdenmbaquirandevwork@gmail.com>
2026-06-25 12:35:55 +0800

    chore: cleanup

--- js/localStorage.js
+++
@@ -1,7 +0,0 @@
-export const getNotes = () => {
-  return JSON.parse(localStorage.getItem("notes")) || [];
-};
-
-export const saveNotes = (notes) => {
-  localStorage.setItem("notes", JSON.stringify(notes));
-};
